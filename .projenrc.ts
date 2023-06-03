455f67f83d37e9a82a4a9819c63bc208682f15c0
Philip M. Gollucci (local) <pgollucci@jlhewitt.local>
2023-06-03 11:16:25 -0400

    feat: adds domains

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,28 @@
-import { awscdk } from 'projen';
+import { awscdk } from "projen";
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
-  name: 'p6-domains',
-  projenrcTs: true,
+  name: "p6-domains",
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  authorEmail: "pgollucci@p6m7g8.com",
+  authorName: "Philip M. Gollucci",
+  authorUrl: "https://www.linkedin.com/in/pgollucci",
+  authorOrganization: true,
+
+  repository: "https://github.com/p6m7g8/p6-domains",
+  stability: "experimental",
+
+  description: "P6: Creates domains from a yaml file in Route53",
+  keywords: ["aws", "cdk", "dns", "domains", "route53", "p6"],
+
+  cdkVersion: "2.81.0",
+  defaultReleaseBranch: "main",
+  projenrcTs: true,
+  releaseFailureIssue: true,
+  prettier: true,
+  autoApproveUpgrades: true,
+  autoApproveOptions: {
+    allowedUsernames: ["p6m7g8-automation"],
+  },
+
+  deps: ["fs@^0.0.2", "@types/js-yaml", "js-yaml"],
 });
 project.synth();
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,68 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as fs from "fs";
+import * as cdk from "aws-cdk-lib";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import { Construct } from "constructs";
 
-export class MyStack extends Stack {
-  constructor(scope: Construct, id: string, props: StackProps = {}) {
+import * as yaml from "js-yaml";
+
+// Configuration file
+const CONFIG_FILE = "conf/domains.yml";
+
+/**
+ *
+ * Domain object in the config file
+ */
+interface Domain {
+  // name of the domain
+  name: string;
+
+  // description of the domain
+  description: string;
+}
+
+/**
+ *
+ * @param filePath
+ * @returns
+ */
+function parseYamlFile(filePath: string): Domain[] {
+  const fileContents = fs.readFileSync(filePath, "utf8");
+  const yamlData = yaml.load(fileContents) as Domain[];
+  return yamlData;
+}
+
+/**
+ *
+ *
+ */
+export class MyStack extends cdk.Stack {
+  /**
+   *
+   * @param scope
+   * @param id
+   * @param props
+   */
+  constructor(scope: Construct, id: string, props: cdk.StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    const domains: Domain[] = parseYamlFile(CONFIG_FILE);
+
+    domains.forEach((domain: Domain) => {
+      new route53.PublicHostedZone(this, domain.name, {
+        zoneName: domain.name,
+        comment: domain.description,
+      });
+    });
   }
 }
 
-// for development, use account/region from cdk cli
-const devEnv = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
+// the AwsEnvironment
+const theEnv = {
+  account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION,
 };
 
-const app = new App();
-
-new MyStack(app, 'p6-domains-dev', { env: devEnv });
-// new MyStack(app, 'p6-domains-prod', { env: prodEnv });
-
+// create the app and stack
+const app = new cdk.App();
+new MyStack(app, "p6-domains", { env: theEnv });
 app.synth();
